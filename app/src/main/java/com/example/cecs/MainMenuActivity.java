4d6eb4e61385cf40ff652e581d43ce61a1665baa
Benjamin Seo <benseo000@gmail.com>
2020-04-28 23:54:29 -0700

    V2

--- app/src/main/java/com/example/cecs/MainMenuActivity.java
+++ app/src/main/java/com/example/cecs/MainMenuActivity.java
@@ -3,16 +3,30 @@ package com.example.cecs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    private static final int REQUEST_CODE_QUIZ = 1;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String KEY_HIGHSCORE = "keyHighScore";
+
+    private TextView tvHighScore;
+
+    private int highscore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        tvHighScore = findViewById(R.id.highscoreTV);
+        loadHighScore();
 
         Button buttonStartQuiz = findViewById(R.id.menuTakeQuizBtn);
         buttonStartQuiz.setOnClickListener(new View.OnClickListener(){
@@ -25,6 +39,36 @@ public class MainMenuActivity extends AppCompatActivity {
 
     private void startQuiz(){
         Intent intent = new Intent(MainMenuActivity.this, QuizActivity.class);
-        startActivity(intent);
+        startActivityForResult(intent, REQUEST_CODE_QUIZ);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_QUIZ) {
+            if (resultCode == RESULT_OK) {
+                int score = data.getIntExtra(QuizActivity.EXTRA_SCORE, 0);
+                if (score > highscore) {
+                    updateHighScore(score);
+                }
+            }
+        }
+    }
+
+    private void loadHighScore() {
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        highscore = prefs.getInt(KEY_HIGHSCORE, 0);
+        tvHighScore.setText("Highscore: " + highscore);
+    }
+
+    private void updateHighScore(int highscoreNew) {
+        highscore = highscoreNew;
+        tvHighScore.setText("Highscore: " + highscore);
+
+        SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_HIGHSCORE, highscore);
+        editor.apply();
     }
 }
--- app/src/main/java/com/example/cecs/QuizActivity.java
+++ app/src/main/java/com/example/cecs/QuizActivity.java
@@ -2,13 +2,218 @@ package com.example.cecs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
 
 public class QuizActivity extends AppCompatActivity {
+    public static final String EXTRA_SCORE = "extraScore";
+    private static final long COUNTDOWN_IN_MILLIS = 30000;
+
+    private TextView tvQuestion;
+    private TextView tvScore;
+    private TextView tvQuestionCount;
+    private TextView tvCountDown;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private Button btnConfirmNext;
+
+    private ColorStateList textColorDefaultRB;
+    private ColorStateList textColorDefaultCD;
+
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMillis;
+
+    private List<Question> questionList;
+    private int questionCounter;
+    private int questionCountTotal;
+    private Question currentQuestion;
+
+    private int score;
+    private boolean answered;
+
+    private long backPressedTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        tvQuestion = findViewById(R.id.textViewQuestion);
+        tvScore = findViewById(R.id.textViewScore);
+        tvQuestionCount = findViewById(R.id.textViewQuestionCount);
+        tvCountDown = findViewById(R.id.textViewCountDown);
+        rbGroup = findViewById(R.id.radioGroup);
+        rb1 = findViewById(R.id.radioButton1);
+        rb2 = findViewById(R.id.radioButton2);
+        rb3 = findViewById(R.id.radioButton3);
+        btnConfirmNext = findViewById(R.id.confirmBtn);
+
+        textColorDefaultRB = rb1.getTextColors();
+        textColorDefaultCD = tvCountDown.getTextColors();
+
+        QuizDBHelper dbHelper = new QuizDBHelper(this);
+        questionList = dbHelper.getAllQuestions();
+        questionCountTotal = questionList.size();
+        Collections.shuffle(questionList);
+
+        showNextQuestion();
+
+        btnConfirmNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if(!answered) {
+                    if (rb1.isChecked() || rb2.isChecked() || rb3.isChecked()) {
+                        checkAnswer();
+                    } else {
+                        Toast.makeText(QuizActivity.this, "Please Select an Answer", Toast.LENGTH_LONG);
+                    }
+                } else {
+                    showNextQuestion();
+                }
+            }
+        });
     }
+
+    private void showNextQuestion() {
+        rb1.setTextColor(textColorDefaultRB);
+        rb2.setTextColor(textColorDefaultRB);
+        rb3.setTextColor(textColorDefaultRB);
+        rbGroup.clearCheck();
+
+        if (questionCounter < questionCountTotal){
+            currentQuestion = questionList.get(questionCounter);
+
+            tvQuestion.setText(currentQuestion.getQuestion());
+            rb1.setText(currentQuestion.getOption1());
+            rb2.setText(currentQuestion.getOption2());
+            rb3.setText(currentQuestion.getOption3());
+
+            questionCounter++;
+            tvQuestionCount.setText("Question: " + questionCounter + "/" + questionCountTotal);
+            answered = false;
+            btnConfirmNext.setText("Confirm");
+
+            timeLeftInMillis = COUNTDOWN_IN_MILLIS;
+            startCountDown();
+        } else {
+            finishQuiz();
+        }
+    }
+
+    private void startCountDown() {
+        countDownTimer = new CountDownTimer(timeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                timeLeftInMillis = 0;
+                updateCountDownText();
+                checkAnswer();
+            }
+        }.start();
+    }
+
+    private void updateCountDownText() {
+        int minutes = (int) (timeLeftInMillis / 1000) / 60;
+        int seconds = (int) (timeLeftInMillis / 1000) % 60;
+
+        String timeFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        tvCountDown.setText(timeFormatted);
+
+        if (timeLeftInMillis < 10000) {
+            tvCountDown.setTextColor(Color.RED);
+        } else {
+            tvCountDown.setTextColor(textColorDefaultCD);
+        }
+    }
+
+    private void checkAnswer() {
+        answered = true;
+
+        countDownTimer.cancel();
+
+        RadioButton rbSelected = findViewById(rbGroup.getCheckedRadioButtonId());
+        int answerNumber = rbGroup.indexOfChild(rbSelected) + 1;
+
+        if(answerNumber == currentQuestion.getAnswerNumber()) {
+            score++;
+            tvScore.setText("Score: " + score);
+        }
+
+        showSolution();
+    }
+
+    private void showSolution() {
+        rb1.setTextColor(Color.RED);
+        rb2.setTextColor(Color.RED);
+        rb3.setTextColor(Color.RED);
+
+        switch (currentQuestion.getAnswerNumber()) {
+            case 1:
+                rb1.setTextColor(Color.GREEN);
+                tvQuestion.setText("Answer 1 is correct");
+                break;
+            case 2:
+                rb2.setTextColor(Color.GREEN);
+                tvQuestion.setText("Answer 2 is correct");
+                break;
+            case 3:
+                rb1.setTextColor(Color.GREEN);
+                tvQuestion.setText("Answer 3 is correct");
+                break;
+        }
+
+        if (questionCounter < questionCountTotal) {
+            btnConfirmNext.setText("Next");
+        } else {
+            btnConfirmNext.setText("Finish");
+        }
+    }
+
+    public void finishQuiz() {
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra(EXTRA_SCORE, score);
+        setResult(RESULT_OK);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(backPressedTime + 2000 > System.currentTimeMillis()) {
+            finishQuiz();
+        } else  {
+            Toast.makeText(this, "Press back again to finish", Toast.LENGTH_SHORT).show();
+        }
+
+        backPressedTime = System.currentTimeMillis();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+    }
+
 }
